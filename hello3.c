841f6cf4c59b74bbd1b658b211058a71174bf053
Diego Sarzi <diegosarzi@estudiorodapiao.com.br>
2013-08-12 14:20:41 -0300

    Outro teste em C de Hello

---
+++ hello3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int i = 1;
+
+int
+main(void)
+{
+
+   if (i == 0)
+ {
+	GetString();
+	printf("Hello, World\n");
+ }
+   else
+ {
+	printf("Variavel diferente de 0");
+ }
+
+}
