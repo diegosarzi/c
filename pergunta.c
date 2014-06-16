fddec0d25b28d5dcb478ac4a1ecde17d62d3a234
gnewlinux <gnewlinux@gmail.com>
2014-06-15 23:32:23 -0300

    ?

---
+++ pergunta.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int num1 = 10;
+	int num2 = 20;
+
+	int resposta;
+
+	num1 < num2 ? printf("sim\n") : printf("não\n");
+
+	num1 < num2 ? resposta = 10 : resposta = -10;
+
+	printf("%i\n", resposta);
+
+
+}
