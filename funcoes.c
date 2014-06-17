8c0e4a4250c7e9e0a3828874242f3ef69c6d1994
gnewlinux <gnewlinux@gmail.com>
2014-06-16 23:59:44 -0300

    Funcoes com variaveis

--- funcoes.c
+++ funcoes.c
@@ -2,14 +2,29 @@
 
 int main(void) {
 
+	// dizer que existe a função
 	void inserirTexto (void);
 
+	// rodar funcao 3 vezes
 	inserirTexto();
+	inserirTexto();
+	inserirTexto();
+	
+	return 0;
 
 }
 
 void inserirTexto (void) {
 
-	printf("Teste, função. \n");
+	// criar variavel local automatica
+	int variavelLocalAutomatica = 2;
+	variavelLocalAutomatica *= 2;
+
+	// criar variavel local estatica 
+	static int variavelLocalEstatica = 2;
+	variavelLocalEstatica *= 2;
+
+	printf("Variavel Local Automatica. %i \n", variavelLocalAutomatica);
+	printf("Variavel Local Estatica = %i \n", variavelLocalEstatica);
 
 }
