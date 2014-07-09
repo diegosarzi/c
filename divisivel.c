668a639cae23d8c1d06ee5660d4aed67938b310e
gnewlinux <gnewlinux@gmail.com>
2014-07-08 22:15:43 -0300

    Divisao sobra %

--- divisivel.c
+++ divisivel.c
@@ -9,6 +9,7 @@ for(i = 1; i <= 20; ++i){
 	// i % 3 == 0 // % significa a sobra da divisao == valor
 	if(i % 3 == 0 || i % 9 == 0){
 		printf("%i - Número divisivel por 3 ou 9\n", i);
+		break;
 	} else {
 		printf("%i - Não divisivel por 3 ou 9\n", i);
 	}
